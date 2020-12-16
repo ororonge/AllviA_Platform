3a9b3775e57af0bf6d784375f9775e9be779f8ce
samsung <samsung@112.172.225.84>
2020-12-16 23:24:17 +0900

    dev

--- LMSCloud_WebMain/src/main/java/com/platform/api/MemberClient.java
+++ LMSCloud_WebMain/src/main/java/com/platform/api/MemberClient.java
@@ -3,7 +3,7 @@ package com.platform.api;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name="LMSCloud_API", url="http://localhost:8401")
+@FeignClient(name="api-main", url="http://192.168.135.128:8480")
 public interface MemberClient {
 	@GetMapping("/api/member/memberList")
     String testFeign();
