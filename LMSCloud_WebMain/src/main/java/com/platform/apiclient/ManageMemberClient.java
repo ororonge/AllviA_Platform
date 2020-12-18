bce069b9a0b2217bf8a8abec6db881a444068ab3
samsung <samsung@112.172.225.84>
2020-12-18 21:53:49 +0900

    dev

--- LMSCloud_WebMain/src/main/java/com/platform/apiclient/ManageMemberClient.java
+++ LMSCloud_WebMain/src/main/java/com/platform/apiclient/ManageMemberClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import com.platform.apiclient.member.model.ManageMember;
 
-@FeignClient(name="api-main", url="http://localhost:8401")
+@FeignClient(name="api-main"/*, url="http://localhost:8401"*/)
 public interface ManageMemberClient {
 	@GetMapping("/api/member/manage/memberList")
 	List<ManageMember> memberList();
