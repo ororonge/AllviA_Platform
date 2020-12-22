943847bf8f0755f564da417769afe4ecda4da757
samsung <samsung@112.172.225.84>
2020-12-22 12:03:26 +0900

    dev

--- LMSCloud_Auth/src/main/java/com/platform/authentication/authorization/CustomAuthenticationProvider.java
+++ LMSCloud_Auth/src/main/java/com/platform/authentication/authorization/CustomAuthenticationProvider.java
@@ -1,20 +1,16 @@
 package com.platform.authentication.authorization;
 
-import java.util.List;
-
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import com.platform.authentication.model.ManagementLoginDTO;
-import com.platform.authentication.token.JwtUtil;
 import com.platform.authentication.token.RedisTokenUtil;
 
 @Component
@@ -25,9 +21,6 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
 	
     @Autowired
     private CustomPasswordEncoder encoder;
-    
-    @Autowired
-    private JwtUtil jwtUtil;
     
     @Autowired
     private RedisTokenUtil redisTokenUtil;
